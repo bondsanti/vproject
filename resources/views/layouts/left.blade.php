d65957972432ef95ede84aa8cf360988543c62c9
santi.c <santi.chpy@gmail.com>
2024-07-26 21:48:35 +0700

    picProfile

--- resources/views/layouts/left.blade.php
+++ resources/views/layouts/left.blade.php
@@ -5,7 +5,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="{{ $dataUserLogin['fileExists'] ? $dataUserLogin['remoteFile'] : url('uploads/avatar.png') }}" class="img-circle" alt="User Image">
+          <img src=" {{ url('uploads/avatar.png') }}" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>คุณ {{ $dataUserLogin['apiData']['data']['name_th'] }}</p>
